3d5c8f435eb5442877f2d74a85f64b05d5a7b4df
zhangyugege <773772174@qq.com>
2016-04-16 17:29:10 +0800

    mi

---
+++ resources.js
@@ -0,0 +1,450 @@
+/**
+ * Created by Administrator on 2015/3/28.
+ */
+
+
+var res = {
+
+    //opening
+    begin:{
+
+        bg:"res/begin-btn/begin-bg.png",
+        biaoti:"res/begin-btn/biaoti.png",
+        enemy:"res/begin-btn/enemy.png",
+        playBtn: 'res/begin-btn/play-btn.png',
+         playBtnS: 'res/begin-btn/play-btn-s.png'
+    },
+    house:{
+        1001:"res/house-anim/1001.jpg",
+        1002:"res/house-anim/1002.jpg",
+        1003:"res/house-anim/1003.jpg",
+        1004:"res/house-anim/1004.jpg",
+        1005:"res/house-anim/1005.jpg",
+        1006:"res/house-anim/1006.jpg",
+        1007:"res/house-anim/1007.jpg",
+        college001:"res/house-anim/college001.png",
+        college002:"res/house-anim/college002.png",
+        college003:"res/house-anim/college003.png",
+        college004:"res/house-anim/college004.png",
+        college005:"res/house-anim/college005.png",
+        college006:"res/house-anim/college006.png",
+        college007:"res/house-anim/college007.png",
+        college008:"res/house-anim/college008.png",
+        college009:"res/house-anim/college009.png",
+        college010:"res/house-anim/college010.png",
+        college011:"res/house-anim/college011.png",
+        college012:"res/house-anim/college012.png",
+        college013:"res/house-anim/college013.png",
+        college014:"res/house-anim/college014.png",
+        college015:"res/house-anim/college015.png",
+        college016:"res/house-anim/college016.png",
+        college017:"res/house-anim/college017.png",
+        college018:"res/house-anim/college018.png",
+        college019:"res/house-anim/college019.png",
+        college020:"res/house-anim/college020.png",
+        college021:"res/house-anim/college021.png",
+        college022:"res/house-anim/college022.png",
+        college023:"res/house-anim/college023.png",
+        college024:"res/house-anim/college024.png",
+        college025:"res/house-anim/college025.png",
+        college026:"res/house-anim/college026.png",
+        college027:"res/house-anim/college027.png",
+        college028:"res/house-anim/college028.png",
+        college029:"res/house-anim/college029.png",
+        college030:"res/house-anim/college030.png",
+        dontlikesleep001:"res/house-anim/dontlikesleep001.png",
+        dontlikesleep002:"res/house-anim/dontlikesleep002.png",
+        dontlikesleep003:"res/house-anim/dontlikesleep003.png",
+        dontlikesleep004:"res/house-anim/dontlikesleep004.png",
+        dontlikesleep005:"res/house-anim/dontlikesleep005.png",
+        dontlikesleep006:"res/house-anim/dontlikesleep006.png",
+        dontlikesleep007:"res/house-anim/dontlikesleep007.png",
+        dontlikesleep008:"res/house-anim/dontlikesleep008.png",
+        dontlikesleep009:"res/house-anim/dontlikesleep009.png",
+        dontlikesleep010:"res/house-anim/dontlikesleep010.png",
+        dontlikesleep011:"res/house-anim/dontlikesleep011.png",
+        dontlikesleep012:"res/house-anim/dontlikesleep012.png",
+        dontlikesleep013:"res/house-anim/dontlikesleep013.png",
+        doudou001:"res/house-anim/doudou001.png",
+        doudou002:"res/house-anim/doudou002.png",
+        doudou003:"res/house-anim/doudou003.png",
+        doudou004:"res/house-anim/doudou004.png",
+        doudou005:"res/house-anim/doudou005.png",
+        doudou006:"res/house-anim/doudou006.png",
+        doudou007:"res/house-anim/doudou007.png",
+        doudou008:"res/house-anim/doudou008.png",
+        doudou009:"res/house-anim/doudou009.png",
+        eating1:"res/house-anim/eating1.png",
+        eating2:"res/house-anim/eating2.png",
+        eating3:"res/house-anim/eating3.png",
+        eating4:"res/house-anim/eating4.png",
+        eating5:"res/house-anim/eating5.png",
+        eating6:"res/house-anim/eating6.png",
+        eating7:"res/house-anim/eating7.png",
+        eating8:"res/house-anim/eating8.png",
+        eating9:"res/house-anim/eating9.png",
+        eating10:"res/house-anim/eating10.png",
+        eating11:"res/house-anim/eating11.png",
+        eating12:"res/house-anim/eating12.png",
+        eating13:"res/house-anim/eating13.png",
+        eating14:"res/house-anim/eating14.png",
+        eating15:"res/house-anim/eating15.png",
+        eating16:"res/house-anim/eating16.png",
+        eating017:"res/house-anim/eating017.png",
+        eating018:"res/house-anim/eating018.png",
+        eating019:"res/house-anim/eating019.png",
+        eating020:"res/house-anim/eating020.png",
+        eating021:"res/house-anim/eating021.png",
+        eating022:"res/house-anim/eating022.png",
+        eating023:"res/house-anim/eating023.png",
+        eating024:"res/house-anim/eating024.png",
+        eating025:"res/house-anim/eating025.png",
+        eating026:"res/house-anim/eating026.png",
+        eating027:"res/house-anim/eating027.png",
+        eating028:"res/house-anim/eating028.png",
+        eating029:"res/house-anim/eating029.png",
+        eating030:"res/house-anim/eating030.png",
+        eating031:"res/house-anim/eating031.png",
+        eating032:"res/house-anim/eating032.png",
+        eating033:"res/house-anim/eating033.png",
+        eating034:"res/house-anim/eating034.png",
+        eating035:"res/house-anim/eating035.png",
+        eating036:"res/house-anim/eating036.png",
+        eating037:"res/house-anim/eating037.png",
+        eating038:"res/house-anim/eating038.png",
+        eating039:"res/house-anim/eating039.png",
+        eating040:"res/house-anim/eating040.png",
+        eating041:"res/house-anim/eating041.png",
+        eating042:"res/house-anim/eating042.png",
+        eating043:"res/house-anim/eating043.png",
+        eating044:"res/house-anim/eating044.png",
+        goodeat001:"res/house-anim/goodeat001.png",
+        goodeat002:"res/house-anim/goodeat002.png",
+        goodeat003:"res/house-anim/goodeat003.png",
+        goodeat004:"res/house-anim/goodeat004.png",
+        goodeat005:"res/house-anim/goodeat005.png",
+        goodeat006:"res/house-anim/goodeat006.png",
+        goodeat007:"res/house-anim/goodeat007.png",
+        goodeat008:"res/house-anim/goodeat008.png",
+        goodeat009:"res/house-anim/goodeat009.png",
+        guai001:"res/house-anim/guai001.png",
+        guai002:"res/house-anim/guai002.png",
+        guai003:"res/house-anim/guai003.png",
+        guai004:"res/house-anim/guai004.png",
+        guai005:"res/house-anim/guai005.png",
+        guai006:"res/house-anim/guai006.png",
+        guai007:"res/house-anim/guai007.png",
+        guai008:"res/house-anim/guai008.png",
+        guai009:"res/house-anim/guai009.png",
+        guai010:"res/house-anim/guai010.png",
+        guai011:"res/house-anim/guai011.png",
+        guai012:"res/house-anim/guai012.png",
+        guai013:"res/house-anim/guai013.png",
+        sleep01:"res/house-anim/insleep01.png",
+        sleep02:"res/house-anim/insleep02.png",
+        sleep03:"res/house-anim/insleep03.png",
+        sleep04:"res/house-anim/insleep04.png",
+        sleep05:"res/house-anim/insleep05.png",
+        sleep06:"res/house-anim/insleep06.png",
+        sleep07:"res/house-anim/insleep07.png",
+        sleep08:"res/house-anim/insleep08.png",
+        sleep09:"res/house-anim/insleep09.png",
+        sleep010:"res/house-anim/insleep010.png",
+        sleep011:"res/house-anim/insleep011.png",
+        sleep012:"res/house-anim/insleep012.png",
+        sleep013:"res/house-anim/insleep013.png",
+        sleep014:"res/house-anim/insleep014.png",
+        sleep015:"res/house-anim/insleep015.png",
+        inclass001:"res/house-anim/inclass001.png",
+        inclass002:"res/house-anim/inclass002.png",
+        inclass003:"res/house-anim/inclass003.png",
+        inclass004:"res/house-anim/inclass004.png",
+        inclass005:"res/house-anim/inclass005.png",
+        inclass006:"res/house-anim/inclass006.png",
+        inclass007:"res/house-anim/inclass007.png",
+        inclass008:"res/house-anim/inclass008.png",
+        inclass009:"res/house-anim/inclass009.png",
+        inclass010:"res/house-anim/inclass010.png",
+        inclass011:"res/house-anim/inclass011.png",
+        inclass012:"res/house-anim/inclass012.png",
+        inclass013:"res/house-anim/inclass013.png",
+        inclass014:"res/house-anim/inclass014.png",
+        inclass015:"res/house-anim/inclass015.png",
+        inclass016:"res/house-anim/inclass016.png",
+        inclass017:"res/house-anim/inclass017.png",
+        inclass018:"res/house-anim/inclass018.png",
+        inclass019:"res/house-anim/inclass019.png",
+        inclass020:"res/house-anim/inclass020.png",
+        inclass021:"res/house-anim/inclass021.png",
+        inclass022:"res/house-anim/inclass022.png",
+        inclass023:"res/house-anim/inclass023.png",
+        inclass024:"res/house-anim/inclass024.png",
+        inclass025:"res/house-anim/inclass025.png",
+        inclass026:"res/house-anim/inclass026.png",
+        inclass027:"res/house-anim/inclass027.png",
+        inclass028:"res/house-anim/inclass028.png",
+        inclass029:"res/house-anim/inclass029.png",
+        kanhua001:"res/house-anim/kanhua001.png",
+        kanhua002:"res/house-anim/kanhua002.png",
+        kanhua003:"res/house-anim/kanhua003.png",
+        kanhua004:"res/house-anim/kanhua004.png",
+        kanhua005:"res/house-anim/kanhua005.png",
+        kanhua006:"res/house-anim/kanhua006.png",
+        kanhua007:"res/house-anim/kanhua007.png",
+        kanhua008:"res/house-anim/kanhua008.png",
+        kanhua009:"res/house-anim/kanhua009.png",
+        kanhua010:"res/house-anim/kanhua010.png",
+        kanhua011:"res/house-anim/kanhua011.png",
+        kanhua012:"res/house-anim/kanhua012.png",
+        kanhua013:"res/house-anim/kanhua013.png",
+        kanhua014:"res/house-anim/kanhua014.png",
+        kanhua015:"res/house-anim/kanhua015.png",
+        kanhua016:"res/house-anim/kanhua016.png",
+        keai001:"res/house-anim/keai001.png",
+        keai002:"res/house-anim/keai002.png",
+        keai003:"res/house-anim/keai003.png",
+        keai004:"res/house-anim/keai004.png",
+        keai005:"res/house-anim/keai005.png",
+        keai006:"res/house-anim/keai006.png",
+        keai007:"res/house-anim/keai007.png",
+        keai008:"res/house-anim/keai008.png",
+        naonao001:"res/house-anim/naonao001.png",
+        naonao002:"res/house-anim/naonao002.png",
+        naonao003:"res/house-anim/naonao003.png",
+        naonao004:"res/house-anim/naonao004.png",
+        naonao005:"res/house-anim/naonao005.png",
+        naonao006:"res/house-anim/naonao006.png",
+        naonao007:"res/house-anim/naonao007.png",
+        naonao008:"res/house-anim/naonao008.png",
+        naonao009:"res/house-anim/naonao009.png",
+        naonao010:"res/house-anim/naonao010.png",
+        naonao011:"res/house-anim/naonao011.png",
+        naonao012:"res/house-anim/naonao012.png",
+        naonao013:"res/house-anim/naonao013.png",
+        naonao014:"res/house-anim/naonao014.png",
+        naonao015:"res/house-anim/naonao015.png",
+        naonao016:"res/house-anim/naonao016.png",
+        naonao017:"res/house-anim/naonao017.png",
+        play001:"res/house-anim/play001.png",
+        play002:"res/house-anim/play002.png",
+        play003:"res/house-anim/play003.png",
+        play004:"res/house-anim/play004.png",
+        play005:"res/house-anim/play005.png",
+        play006:"res/house-anim/play006.png",
+        play007:"res/house-anim/play007.png",
+        play008:"res/house-anim/play008.png",
+        qingwa001:"res/house-anim/qingwa001.png",
+        qingwa002:"res/house-anim/qingwa002.png",
+        qingwa003:"res/house-anim/qingwa003.png",
+        qingwa004:"res/house-anim/qingwa004.png",
+        qingwa005:"res/house-anim/qingwa005.png",
+        qingwa006:"res/house-anim/qingwa006.png",
+        qingwa007:"res/house-anim/qingwa007.png",
+        qingwa008:"res/house-anim/qingwa008.png",
+        qingwa009:"res/house-anim/qingwa009.png",
+        qingwa010:"res/house-anim/qingwa010.png",
+        xihuanxiaoshou001:"res/house-anim/xihuanxiaoshou001.png",
+        xihuanxiaoshou002:"res/house-anim/xihuanxiaoshou002.png",
+        xihuanxiaoshou003:"res/house-anim/xihuanxiaoshou003.png",
+        xihuanxiaoshou004:"res/house-anim/xihuanxiaoshou004.png",
+        xihuanxiaoshou005:"res/house-anim/xihuanxiaoshou005.png",
+        xihuanxiaoshou006:"res/house-anim/xihuanxiaoshou006.png",
+        xihuanxiaoshou007:"res/house-anim/xihuanxiaoshou007.png",
+        xihuanxiaoshou008:"res/house-anim/xihuanxiaoshou008.png",
+        xihuanxiaoshou009:"res/house-anim/xihuanxiaoshou009.png",
+        xihuanxiaoshou010:"res/house-anim/xihuanxiaoshou010.png",
+
+
+
+        housebg:"res/house-btn/house-bg.png",
+        xiaoshou:"res/house-btn/xiaoshou.png",
+        tybtnmenu:"res/house-btn/tybtnmenu.png",
+        sleepbtn:"res/house-btn/sleep-btn.png",
+        playbtn:"res/house-btn/play-btn.png",
+        gotoschoolbtn:"res/house-btn/gotoschool-btn.png",
+        goodeatbtn:"res/house-btn/goodeat-btn.png",
+        eatbtn:"res/house-btn/eat-btn.png",
+        doudoubtn:"res/house-btn/doudou-btn.png",
+        collegebtn:"res/house-btn/college-btn.png",
+        thinking:"res/house-btn/thinking.png"
+
+
+    },
+    menu: {
+        //bg: 'res/menu-bg.png',
+        //
+        //storeBtn: 'res/rank-btn.png',
+        //storeBtnS: 'res/rank-btn.png',
+        //setBtn: 'res/set-btn.png',
+        //setBtnS: 'res/set-btn-s.png',
+        //startBtn: 'res/start-btn-normal.png',
+        //startBtnS: 'res/start-btn-selected.png',
+        //aboutBtn: 'res/about-btn.png',
+        //aboutBtnS: 'res/about-btn-s.png',
+        //logo: 'res/game-logo.png',
+        //wait: 'res/wait.png',
+
+
+
+
+        menubg:"res/menu-selected/menu-bg.png",
+        menu_handbook:"res/menu-selected/menu_handbook.png",
+        menu_myhouse:"res/menu-selected/menu_myhouse.png",
+        menu_myhouse_selected:"res/menu-selected/menu_myhouse_selected.png",
+        menuselected1:"res/menu-selected/menu-selected-1.png",
+        menuselected2:"res/menu-selected/menu-selected-2.png",
+        menuselected3:"res/menu-selected/menu-selected-3.png",
+        menuselected4:"res/menu-selected/menu-selected-4.png",
+        pageUpLeft:"res/menu-selected/pageUpLeft.png",
+        pageUpRight:"res/menu-selected/pageUpRight.png"
+
+
+
+    },
+    main:{
+
+        farbg:"res/main-btn/far-bg.png",
+        enemy0:"res/main-btn/enemy-0.png",
+        enemy01:"res/main-btn/enemy-01.png",
+        enemy1:"res/main-btn/enemy-1.png",
+        enemy2:"res/main-btn/enemy-2.png",
+        fog:"res/main-btn/fog.png",
+        jiange:"res/main-btn/jiange.png",
+        particle:"res/main-btn/particle.png",
+        starLight:"res/main-btn/starLight.png",
+        progressnei:"res/main-btn/progress-nei.png",
+        progresswai:"res/main-btn/progress-wai.png",
+        goleft:"res/main-btn/goleft.png",
+        goright:"res/main-btn/goright.png",
+        maintishilogo:"res/main-btn/main-tishi-logo.png",
+        maintishiguaiwubg:"res/main-btn/main-tishi-guaiwubg.png",
+        maintishibg:"res/main-btn/main-tishi-bg.png",
+        bomb:"res/main-btn/bomb.png"
+
+
+
+    },
+
+    over:{
+
+
+        menu: 'res/over-btn/menu.png',
+        winligt:"res/over-btn/winlight.png",
+        loseligt:"res/over-btn/loselight.png",
+        wintext:"res/over-btn/wintext.png",
+        losetext:"res/over-btn/losetext.png",
+        reload:"res/over-btn/reload.png",
+        scoreboard2:"res/over-btn/score-board2.png",
+        scoreboard:"res/over-btn/score-board.png",
+        nextlevel:"res/over-btn/store.png",
+        store: 'res/over-btn/store1.png'
+
+    },
+    pause:{
+        pauseboard:"res/pause/pauseboard.png",
+        playbtn:"res/pause/play-btn.png"
+    },
+
+
+    Bubble_0:"res/main-btn/0.png",
+    Bubble_1:"res/main-btn/1.png",
+    Bubble_2:"res/main-btn/2.png",
+    Bubble_3:"res/main-btn/3.png",
+    Bubble_4:"res/main-btn/4.png",
+    Bubble_5:"res/main-btn/5.png",
+    Bubble_6:"res/main-btn/6.png",
+    Shooter:"res/main-btn/bubble_shooter.png",
+    kapai_1:"res/main-btn/kapai-1.png",
+    kapai_2:"res/main-btn/kapai-2.png",
+    kapai_3:"res/main-btn/kapai-3.png",
+
+
+    open: {
+        bg: 'res/open-bg.png',
+        team: 'res/maxon-team.png'
+    },
+    /////////////////////
+    //mode: {
+    //    board: 'res/mode-board.png',
+    //    mode1: 'res/mode1.png',
+    //    mode2: 'res/mode2.png'
+    //},
+    //
+    //info: {
+    //    board: 'res/info-board.png',
+    //},
+    //
+    //rank: {
+    //    board: 'res/rank-board.png'
+    //},
+
+
+
+
+
+
+
+    sound:{
+        //掉落音效
+        combo1:"res/sound/A_combo1.ogg",
+        combo2:"res/sound/A_combo2.ogg",
+        combo3:"res/sound/A_combo3.ogg",
+        combo4:"res/sound/A_combo4.ogg",
+        combo5:"res/sound/A_combo5.ogg",
+
+        Sound_On:"res/sound/sound-on.png",
+        Sound_Off:"res/sound/sound-off.png",
+
+
+
+        //发射音效
+        ground:"res/sound/A_ground.ogg",
+        //碰撞
+        select:"res/sound/A_select.ogg",
+        //menu音效
+        bgGame:"res/sound/bgGame.ogg",
+        //游戏主音效
+        bgWelcome:"res/sound/bgWelcome.ogg",
+        UI_rankup:"res/sound/UI_rankup.ogg",
+        V_ready:"res/sound/V_ready.ogg",
+
+        timi:"res/sound/timi.ogg",
+        lose:"res/sound/lose.ogg",
+        hurt:"res/sound/hurt.ogg"
+
+
+
+    }
+
+    //ui: {
+    //    goldbar: 'res/ui/gold-bar.png',
+    //    energybar: 'res/ui/energy-bar.png',
+    //    progress: 'res/ui/progress.png',
+    //    soundOn: 'res/ui/soundOnBtn.png',
+    //    soundOff: 'res/ui/soundOffBtn.png',
+    //    distance: 'res/ui/distance.png',
+    //    aboutBoard: 'res/ui/about-board.png',
+    //    backBtn: 'res/ui/back-btn.png',
+    //    setBoard: 'res/ui/set-board.png',
+    //    onBtn: 'res/ui/on-btn.png',
+    //    offBtn: 'res/ui/off-btn.png',
+    //    highBtn: 'res/ui/high-btn.png',
+    //    lowBtn: 'res/ui/low-btn.png',
+    //    storeBoard: 'res/ui/store-board.png',
+    //    buy30: 'res/ui/buy-30.png',
+    //    buy50: 'res/ui/buy-50.png',
+    //    magnetProp: 'res/ui/magnet-prop.png',
+    //    shoesProp: 'res/ui/shoes-prop.png',
+    //    redshoesProp: 'res/ui/redshoes-prop.png'
+    //},
+
+
+
+};
+
+
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
